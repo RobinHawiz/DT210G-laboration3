bb4ccb6c78c9db05ce3c073b6b707eac0d217753
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2026-01-31 14:39:39 +0100

    init: scaffold React+TS app with Vite

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import checker from "vite-plugin-checker";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    checker({
+      typescript: true,
+      eslint: {
+        useFlatConfig: true,
+        lintCommand: 'eslint "src/**/*.{ts,tsx}" --cache',
+        dev: {
+          logLevel: ["error"],
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "src"),
+      "@components": path.resolve(import.meta.dirname, "src/components"),
+      "@api": path.resolve(import.meta.dirname, "src/api"),
+      "@hooks": path.resolve(import.meta.dirname, "src/hooks"),
+      "@types": path.resolve(import.meta.dirname, "src/types"),
+    },
+  },
+  server: {
+    open: true,
+  },
+});
