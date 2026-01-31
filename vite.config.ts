feb5711076056e8950ce2f9677b81907f95e4f7c
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2026-01-31 20:17:53 +0100

    chore(utils): add delay() util and @utils alias

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,7 @@ export default defineConfig({
       "@api": path.resolve(import.meta.dirname, "src/api"),
       "@hooks": path.resolve(import.meta.dirname, "src/hooks"),
       "@types": path.resolve(import.meta.dirname, "src/types"),
+      "@utils": path.resolve(import.meta.dirname, "src/types"),
     },
   },
   server: {
