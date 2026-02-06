c9b06d2593aeec946ede51fb1dc2ea58a6f43f70
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2026-02-06 09:09:52 +0100

    fix(items): truncate item text to prevent card growth

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -8,17 +8,19 @@ type Props = {
 
 function Item({ name, description, price, imageUrl, amount }: Props) {
   return (
-    <article className="bg-white overflow-auto rounded-lg shadow-lg w-full">
+    <article className="w-full overflow-auto rounded-lg bg-white shadow-lg">
       <div className="px-4 py-2">
-        <h3 className="text-xl font-bold text-gray-800 uppercase">{name}</h3>
-        <p className="mt-1 text-sm text-gray-600">{description}</p>
+        <h3 className="truncate text-xl font-bold text-gray-800 uppercase">
+          {name}
+        </h3>
+        <p className="mt-1 truncate text-sm text-gray-600">{description}</p>
       </div>
       <img
-        className="object-cover h-38.75 w-38.75 mt-2 mx-auto"
+        className="mx-auto mt-2 h-38.75 w-38.75 object-cover"
         src={imageUrl}
         alt={name}
       />
-      <div className="flex items-center justify-between px-4 py-2 bg-gray-900">
+      <div className="flex items-center justify-between bg-gray-900 px-4 py-2">
         <p className="text-lg font-bold text-white">{amount} st</p>
         <p className="text-lg font-bold text-white">{price} kr</p>
       </div>
